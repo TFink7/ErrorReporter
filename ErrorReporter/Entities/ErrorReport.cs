943fde5f2fbdd7f4cd72ea1dc0c867a48f9dc562
TFink7 <98671947+TFink7@users.noreply.github.com>
2026-02-22 18:07:06 -0500

    Add Unit Tests

--- ErrorReporter/Entities/ErrorReport.cs
+++ ErrorReporter/Entities/ErrorReport.cs
@@ -1,12 +1,21 @@
-﻿namespace ErrorReporter.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ErrorReporter.Entities
 {
     public class ErrorReport
     {
         public int Id { get; set; }
-        public string Service { get; set; }
-        public string Message { get; set; }
+
+        [Required]
+        public required string Service { get; set; }
+
+        [Required]
+        public required string Message { get; set; }
+
         public string? StackTrace { get; set; }
+
         public DateTime OccurredAt { get; set; }
-        public DateTime CreatedAt { get; set; } 
+
+        public DateTime CreatedAt { get; set; }
     }
 }

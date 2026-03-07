c4cb2bcf98d629ed72a7cedc894a80b5111070c1
TFink7 <98671947+TFink7@users.noreply.github.com>
2026-03-07 13:15:52 -0500

    Enhance CreateErrorReportDto with data annotations for validation

--- ErrorReporter/Dtos/CreateErrorReportDto.cs
+++ ErrorReporter/Dtos/CreateErrorReportDto.cs
@@ -1,11 +1,12 @@
-﻿using ErrorReporter.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+using ErrorReporter.Entities;
 
 namespace ErrorReporter.Dtos;
 
 public record CreateErrorReportDto(
-    string Service,
-    string Message,
-    string? StackTrace,
+    [Required, MaxLength(200)] string Service,
+    [Required, MaxLength(1000)] string Message,
+    [MaxLength(4000)] string? StackTrace,
     Severity Severity,
     DateTime OccurredAt
 );
